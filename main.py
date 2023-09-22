7d4d678092a646e2278afa42d481a89d28ffb639
gwen-nathan <145736896+gwen-nathan@users.noreply.github.com>
2023-09-22 03:19:02 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+def on_forever():
+    basic.show_string("GWEN AND NATHAN")
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showString("I LOVE NARRA")
+    basic.showString("WELCOME TO NARRA")
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+})
